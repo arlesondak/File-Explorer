0d6732f9aa821a4a70e9ffe3bf993a4e4f376b34
Andrew Lesondak <andrewlesondak@Drewdaks-Mac.attlocal.net>
2020-07-07 21:51:02 -0700

    creating node server

---
+++ app.js
@@ -0,0 +1,18 @@
+//You will build this project using pure Node to gain a thorough understanding of Node. There are obviously easier ways of building this project using external modules, but that is the purpose of this project as you would miss out on many code concepts.
+
+// require node modules
+const http = require('http');
+
+// file imports
+
+// connection settings
+// if port not found in process.env, then use 3000
+const port = process.env.port || 3000;
+
+// create server
+const server = http.createServer(respond);
+
+// listen to client requests on the specific port, the port should be available
+server.listen(port, () => {
+	console.log(`listening on port: ${port}`);
+});
